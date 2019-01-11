95d05f8fc416c26761bcd30305b4a1890a6f329a
AlexisLinsTool <1402593684@qq.com>
2019-01-11 09:22:13 +0800

    Alexis2019初步建立control

---
+++ app/src/main/java/manager/dc/com/dcoursemanager/MyControl/MyTextView.java
@@ -0,0 +1,20 @@
+package manager.dc.com.dcoursemanager.MyControl;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.widget.TextView;
+
+public class MyTextView extends TextView {
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.GRAY);
+    }
+}
---
+++ app/src/main/java/manager/dc/com/dcoursemanager/UI/MainTable_fragment.java
@@ -0,0 +1,13 @@
+package manager.dc.com.dcoursemanager.UI;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+public class MainTable_fragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
