5fa9a316fd91459b9b1272b6434ec801734c10a8
AlexisLinsTool <1402593684@qq.com>
2019-01-13 10:31:21 +0800

    Alexis2019数据库基本完成

---
+++ app/src/main/java/manager/dc/com/dcoursemanager/OBJ/Courses.java
@@ -0,0 +1,67 @@
+package manager.dc.com.dcoursemanager.OBJ;
+
+public class Courses {
+    private Course w1 = null;
+    private Course w2 = null;
+    private Course w3 = null;
+    private Course w4 = null;
+    private Course w5 = null;
+    private Course w6 = null;
+    private Course w7 = null;
+
+    public Course getW1() {
+        return w1;
+    }
+
+    public void setW1(Course w1) {
+        this.w1 = w1;
+    }
+
+    public Course getW2() {
+        return w2;
+    }
+
+    public void setW2(Course w2) {
+        this.w2 = w2;
+    }
+
+    public Course getW3() {
+        return w3;
+    }
+
+    public void setW3(Course w3) {
+        this.w3 = w3;
+    }
+
+    public Course getW4() {
+        return w4;
+    }
+
+    public void setW4(Course w4) {
+        this.w4 = w4;
+    }
+
+    public Course getW5() {
+        return w5;
+    }
+
+    public void setW5(Course w5) {
+        this.w5 = w5;
+    }
+
+    public Course getW6() {
+        return w6;
+    }
+
+    public void setW6(Course w6) {
+        this.w6 = w6;
+    }
+
+    public Course getW7() {
+        return w7;
+    }
+
+    public void setW7(Course w7) {
+        this.w7 = w7;
+    }
+}
